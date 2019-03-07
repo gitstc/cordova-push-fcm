51d0ece2f7096fd3865607413763fd5241bb219c
user15 <user15@STC.COM>
2019-03-07 19:38:46 +0200

    Update

--- hooks/ios/pod_install.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('child_process');
-
-module.exports = function(context) {
-    console.log("Running pod install");
-    exec('cd ' + context.opts.projectRoot + '/platforms/ios && pod install');
-    console.log("Done");
-}
